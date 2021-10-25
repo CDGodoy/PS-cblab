07f7a7c9341476056283b2a7722b5cfc67cb5ce6
Carlos Nascimento <cdgodoy17@gmail.com>
2021-10-25 15:03:01 -0300

    adicionando outro .jar

--- src/main/java/br/com/godoydev/PScblab/utils/DummyData.java
+++ src/main/java/br/com/godoydev/PScblab/utils/DummyData.java
@@ -16,28 +16,33 @@ public class DummyData {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    //@PostConstruct
+    @PostConstruct
     public void saveUsers(){
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario1 =  new Usuario();
-        usuario1.setNome("testeADM");
-        usuario1.setEmail("teste2@admin.com");
-        usuario1.setSenha(new BCryptPasswordEncoder().encode("admin"));
-        usuario1.setSuperUser(true);
 
-        Usuario usuario2 =  new Usuario();
-        usuario2.setNome("testeUSER");
-        usuario2.setEmail("user2@usuario.com");
-        usuario2.setSenha(new BCryptPasswordEncoder().encode("usuario"));
-        usuario2.setSuperUser(false);
+        if(usuarioRepository.findAll().isEmpty()){
 
-        usuarios.add(usuario1);
-        usuarios.add(usuario2);
+            List<Usuario> usuarios = new ArrayList<>();
+            Usuario usuario1 =  new Usuario();
+            usuario1.setNome("admin");
+            usuario1.setEmail("admin@admin.com");
+            usuario1.setSenha(new BCryptPasswordEncoder().encode("admin"));
+            usuario1.setSuperUser(true);
 
-        for (Usuario usuario : usuarios){
-            Usuario usuarioSalvo = usuarioRepository.save(usuario);
-            System.out.println(usuarioSalvo.getId());
+            Usuario usuario2 =  new Usuario();
+            usuario2.setNome("usuario");
+            usuario2.setEmail("usuario@usuario.com");
+            usuario2.setSenha(new BCryptPasswordEncoder().encode("usuario"));
+            usuario2.setSuperUser(false);
+
+            usuarios.add(usuario1);
+            usuarios.add(usuario2);
+
+            for (Usuario usuario : usuarios){
+                Usuario usuarioSalvo = usuarioRepository.save(usuario);
+                System.out.println(usuarioSalvo.getId());
+            }
         }
+
 
     }
 

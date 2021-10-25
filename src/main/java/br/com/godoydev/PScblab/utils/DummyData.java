5322999f5c8e0ab9ae9f42aef1f760138bd0b774
Carlos Nascimento <cdgodoy17@gmail.com>
2021-10-25 14:46:15 -0300

    ajustando infos

--- src/main/java/br/com/godoydev/PScblab/utils/DummyData.java
+++ src/main/java/br/com/godoydev/PScblab/utils/DummyData.java
@@ -18,26 +18,31 @@ public class DummyData {
 
     @PostConstruct
     public void saveUsers(){
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario1 =  new Usuario();
-        usuario1.setNome("admin");
-        usuario1.setEmail("admin@admin.com");
-        usuario1.setSenha(new BCryptPasswordEncoder().encode("admin"));
-        usuario1.setSuperUser(true);
 
-        Usuario usuario2 =  new Usuario();
-        usuario2.setNome("usuario");
-        usuario2.setEmail("usuario@usuario.com");
-        usuario2.setSenha(new BCryptPasswordEncoder().encode("usuario"));
-        usuario2.setSuperUser(false);
+        if(usuarioRepository.findAll().isEmpty()){
 
-        usuarios.add(usuario1);
-        usuarios.add(usuario2);
+            List<Usuario> usuarios = new ArrayList<>();
+            Usuario usuario1 =  new Usuario();
+            usuario1.setNome("admin");
+            usuario1.setEmail("admin@admin.com");
+            usuario1.setSenha(new BCryptPasswordEncoder().encode("admin"));
+            usuario1.setSuperUser(true);
 
-        for (Usuario usuario : usuarios){
-            Usuario usuarioSalvo = usuarioRepository.save(usuario);
-            System.out.println(usuarioSalvo.getId());
+            Usuario usuario2 =  new Usuario();
+            usuario2.setNome("usuario");
+            usuario2.setEmail("usuario@usuario.com");
+            usuario2.setSenha(new BCryptPasswordEncoder().encode("usuario"));
+            usuario2.setSuperUser(false);
+
+            usuarios.add(usuario1);
+            usuarios.add(usuario2);
+
+            for (Usuario usuario : usuarios){
+                Usuario usuarioSalvo = usuarioRepository.save(usuario);
+                System.out.println(usuarioSalvo.getId());
+            }
         }
+
 
     }
 
